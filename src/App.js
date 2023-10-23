54cd534917a9a41059adf4949d87806804d69503
Trovoada01 <112417747+Trovoada01@users.noreply.github.com>
2023-10-23 11:58:50 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+function App() {   
+  return (
+    <div>
+      <Navbar />
+      <h1>Router</h1>
+      <Outlet />
+      <p>Footer</p>
+    </div>
+  );
+}
+export default App;
---
+++ src/routes/login.jsx
@@ -0,0 +1,18 @@
+import '../css/myForms.css'
+import { link } from 'react-router-dom'
+function myForms(){
+    <div>
+        <form>
+            <label>
+                Usuario:
+                <input type="text" />
+            </label>
+            <label>
+                <input type="text" />
+            </label>
+            <link to="/">Acessar</link>
+        </form>
+    </div>
+}
+
+export default myForms
